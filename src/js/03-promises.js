5961d2ac91849b7d8c30eae4086108105a98dbe5
Оксана Качмар <108275501+Profiscribe@users.noreply.github.com>
2022-09-19 19:05:39 +0300

    Finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,10 +7,22 @@
 //   }
 // }
 import Notiflix from 'notiflix';
+
 const delay = document.querySelector('input[name="delay"]');
 const step = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
 const btnCreatePromise = document.querySelector('button[type="submit"]');
+
+// Напиши скрипт, який на момент сабміту форми викликає функцію
+// createPromise(position, delay) стільки разів, скільки ввели в поле amount.
+// Під час кожного виклику передай їй номер промісу(position), що створюється,
+//   і затримку, враховуючи першу затримку(delay), введену користувачем, і крок(step).
+
+// Доповни код функції createPromise таким чином,
+//   щоб вона повертала один проміс, який виконується або відхиляється через delay часу.
+//   Значенням промісу повинен бути об'єкт, в якому будуть властивості position 
+//   і delay зі значеннями однойменних параметрів.Використовуй 
+// початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
